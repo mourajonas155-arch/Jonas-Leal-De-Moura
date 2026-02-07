ff13a7693714c1aeda22cc50a219364bc103f735
mourajonas155-arch <mourajonas155@gmail.com>
2026-02-07 11:22:57 -0300

    789

---
+++ jantar.py
@@ -0,0 +1,16 @@
+# num = 1
+# while num <= 9:
+#     print(num)
+#     num += 2
+
+# contador = 1
+# while contador <= 5:
+#     print(f'valor:{contador}')
+#     contador += 1
+
+# num = 1
+# somatorio = 0
+# while num <= 5:
+#     somatorio += num
+#     num += 1
+#     print(f'somatório {somatorio}')
