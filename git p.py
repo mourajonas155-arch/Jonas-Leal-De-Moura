93db5af2d0b3da636626e3e7c08470191a1900c4
mourajonas155-arch <mourajonas155@gmail.com>
2026-02-07 09:40:56 -0300

    comin

--- git p.py
+++ git p.py
@@ -1,2 +1,4 @@
 print("Bem vindo ás aulas de sábado letivo")
 print("Hoje vamos aprender sobre python")
+print("Vamos começar com o básico: variáveis, tipos de dados")
+print("Depois, vamos avançar para funções, módulos e bibliotecas")
